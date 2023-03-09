8bfdf35df2656e6a88a1a00f419dfce49701a460
asadhossain <developerasad007@gmail.com>
2023-03-09 21:16:08 +0600

    a

---
+++ form.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>form</title>
+</head>
+<body>
+    
+<Form action="img.php" method="post" enctype="multipart/form-data">
+
+
+<input type="file" name="uplod_image"> <br>
+<input type="submit" name="submit" >
+
+
+
+</Form>
+
+</body>
+</html>
